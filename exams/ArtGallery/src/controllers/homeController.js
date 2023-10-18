dce989ecdbff65d72d2720d86e1f0decaba8a502
Borislav Iliev <85155699+bebo13133@users.noreply.github.com>
2023-10-18 19:00:50 +0300

    Update homeController.js

--- exams/ArtGallery/src/controllers/homeController.js
+++ exams/ArtGallery/src/controllers/homeController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { log } = require('console')
 const artService = require('../services/artService')
-const {isAuth} = require('../middlewares/authMiddleware');
+const { isAuth } = require('../middlewares/authMiddleware');
 const { extractErrorMessage } = require('../utils/errorHelpers')
 const userService = require('../services/userService')
 
@@ -10,34 +10,37 @@ const userService = require('../services/userService')
 
 
 router.get('/', async (req, res) => {
-  try{
-    
-    const arts = await artService.getAll().lean()
-console.log(arts)
+  try {
 
-    res.render('home',{arts})
-    
-  }catch(err) {
+    const arts = await artService.getAll().lean()
+    console.log(arts)
+
+    res.render('home', { arts })
+
+  } catch (err) {
 
     const errorMessage = extractErrorMessage(err)
-        console.log(errorMessage)
+    console.log(errorMessage)
 
-        res.render('/', { error: errorMessage })
+    res.render('/', { error: errorMessage })
   }
 
 });
 
 
-router.get('/profile',isAuth, async(req, res) => {
+router.get('/profile', isAuth, async (req, res) => {
 
-    const user = await userService.findOwner(req.user._id).populate('publications').populate('shares').lean()
-const publication = user.publications.map(x=>x.title).join(", ")
-const sharesTitle = user.shares.map(x=>x.title).join(", ")
-  res.render('profile',{...user,publication, sharesTitle })
+  const user = await userService.findOwner(req.user._id).populate('publications').populate('shares').lean()
+
+  const publication = user.publications.map(x => x.title).join(", ")
+
+  const sharesTitle = user.shares.map(x => x.title).join(", ")
+
+  res.render('profile', { ...user, publication, sharesTitle })
 })
 
 
 router.get('/404', (req, res) => {
-    res.render('404')
+  res.render('404')
 })
 module.exports = router
